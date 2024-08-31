cb7b9e7a4e2c3b78496c03c3019f4892296fa1ab
K. R. <165849060+Quarantining@users.noreply.github.com>
2024-08-31 14:31:15 -0700

    0.1.1 Add user input to pull slot-machine-game.py
    Prompts user to press 'ENTER' to Pull the lever on the slot machine. Game checks if user pressed enter, and then calls function, and returns/prints result. Also organized code slightly better, with vars/functions at the top, and user input at the bottom. If/else statement with minimal error handling.

--- slot-machine-folder/slot-machine-game.py
+++ slot-machine-folder/slot-machine-game.py
@@ -1,12 +1,21 @@
+# 0.1.1
 import random
 
+# slot machine slots function.
 def pull_slots(slots): # argument is any list
-    result = [] # empty list.
-    for i in range(3): # iterates through list 3 times.
+    result = [] # Initialize empty list.
+    for i in range(3): # iterates through list(slots) 3 times.
         item = random.choice(slots) # randomly chooses an item from the list.
         result.append(item) # appends (adds to empty list) the selected item.
     return result # returns the empty list (which now contains 3 randomly selected items).
 
-slots = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
-pull = pull_slots(slots)
-print(pull)
+slots = ["A", "B", "C", "D", "E", "F", "G", "H", "I"] # list of possible slots.
+
+
+user_pull = input("Press 'ENTER' to Pull Lever!") # user input to pull lever
+
+if user_pull == "": # if user presses enter key.
+    pull = pull_slots(slots) # calls function and stores result list as var pull.
+    print(pull) # prints pull
+else: # for errors
+    print("Error")
