f938bb0e220b587278ae2de35340642aee9f9b95
SylphsCavort <64819847+SylphsCavort@users.noreply.github.com>
2024-08-31 14:15:08 -0700

    Added total of 9 symbols.

--- slot-machine-folder/slot-machine-game.py
+++ slot-machine-folder/slot-machine-game.py
@@ -4,9 +4,9 @@ def pull_slots(slots): # argument is any list
     result = [] # empty list.
     for i in range(3): # iterates through list 3 times.
         item = random.choice(slots) # randomly chooses an item from the list.
-        result.append(item) # appends (adds to empty list) the slected item.
+        result.append(item) # appends (adds to empty list) the selected item.
     return result # returns the empty list (which now contains 3 randomly selected items).
 
-slots = ["A", "B", "C"]
+slots = ["A", "B", "C", "D", "E", "F", "G", "H", "I"]
 pull = pull_slots(slots)
 print(pull)
