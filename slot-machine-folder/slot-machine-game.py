7679e33fcc55ae042e5fe76f5d948bf082743e0b
Quarantining <165849060+Quarantining@users.noreply.github.com>
2024-08-31 22:43:58 -0700

    v0.1.6 check_win() function
    check_win() function now uses a dict instead of list to check return value for win.

--- slot-machine-folder/slot-machine-game.py
+++ slot-machine-folder/slot-machine-game.py
@@ -1,10 +1,11 @@
-# version 0.1.5
+# version 0.1.6
 import random
 import os
 
 
 def clear_screen(): # function to clear screen.
     os.system('cls' if os.name == 'nt' else 'clear')
+
 
 # slot machine slots function.
 def pull_slots(slots): # argument is any list
@@ -16,45 +17,42 @@ def pull_slots(slots): # argument is any list
 
 
 def check_win(result): # checks if result is a win or lose.
-    if all(item == result[0] for item in result): # checks if all items are the same as item 0. Itterates through list checking if item is == to item [0].
-        if result[0] == "🍎": # checks which emoji won.
-            print("You win 10$")
-        if result[0] == "🥖":
-            print("You win 100$")
-        if result[0] == "🍒":
-            print("You win 500$")
-        if result[0] == "🍩":
-            print("You win 1000$")
-        if result[0] == "🐘":
-            print("You win 1500$")
-        if result[0] == "🌸":
-            print("You win 2000$")
-        if result[0] == "🍇":
-            print("You win 5000$")
-        if result[0] == "❤️":
-            print("You win 7500$")
-        if result[0] == "🍦":
-            print("You win 10,000$")
+
+    win_message = {
+        "🍎": "You win 10$",
+        "🥖": "You win 50$",
+        "🍒": "You win 100$",
+        "🍩": "You win 500$",
+        "🐘": "You win 750$",
+        "🌸": "You win 1000$",
+        "🍇": "You win 1200$",
+        "❤️": "You win 1500$",
+        "🍦": "You win 2000$"
+    }
+
+    if all(item == result[0] for item in result): # checks if all items are the same as item 0. Iterates through list checking if item is == to item [0].
+        return win_message.get(result[0], f"You won {result[0]}") # If all items are the same, .get correct key and return value.
     else:
-        print("You lose.")
+        return "You didn't win."
 
 
 slots = ["🍎", "🥖", "🍒", "🍩", "🐘", "🌸", "🍇", "❤️", "🍦"] # list of possible slots.
 
 print("Press 'ENTER' to Pull Lever!")
 while True:
+
     user_pull = input("")
     clear_screen() # clears the screen
 
     if user_pull == "": # if user presses enter key.
         print("Press 'ENTER' to Pull Lever!")
-        result = pull_slots(slots) # calls function and stores result list as var pull.
-        print(result)
-        check_win((result))
-
+        slots_result = pull_slots(slots) # calls function and stores result list as var pull.
+        print(slots_result)
+        checks_win = check_win((slots_result))
+        print(checks_win)
     elif user_pull == "stop": # stops program
-        print("Program stopping..")
+        print("Program stopped.")
         break
 
     else: # for errors
-        print("Error")
+        print("Unknown Input")
